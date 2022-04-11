d5b18f9938e5934db712110b906eaea6256eaf3c
admin <505316314@qq.com>
2022-04-11 23:05:49 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const JSX= 
+<React.StrictMode>
+<App />
+</React.StrictMode>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const getItem=document.getElementById('root');
+
+ReactDOM.render(JSX, getItem);
+
 reportWebVitals();
